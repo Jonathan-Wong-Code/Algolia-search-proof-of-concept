49bb8f56127734ef62b25cd27f426697a6a244d4
Jonathan Wong <jonathan.wong@applydigital.co>
2021-02-10 12:27:30 -0500

    Algolia PoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,113 @@
+import algoliasearch from 'algoliasearch/lite';
+import qs from 'qs';
+import { useState } from 'react';
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  Highlight,
+  Stats,
+  SortBy,
+  Pagination,
+} from 'react-instantsearch-dom';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'instantsearch.css/themes/algolia.css';
+import { useHistory } from 'react-router-dom';
 
-function App() {
+const DEBOUNCE_TIME = 400;
+
+const searchClient = algoliasearch(
+  'QHMAVO91S2', // App ID
+  'e32e5c7769c688d52d37bd88e72fb449' // Public Search Key
+);
+
+// Sorting requires a separate index for each sorting option.
+const sortByItems = [
+  {
+    value: 'demo_ecommerce',
+    label: 'Most Relevant',
+  },
+  {
+    value: 'sortby_replica_price_desc',
+    label: 'Highest Price',
+  },
+];
+
+const Hit = ({ hit }: any) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        <Highlight attribute='name' hit={hit} tagName='mark' />
+      </h3>
+      <p>
+        <Highlight attribute='description' hit={hit} tagName='mark' />
+      </p>
+      <p>Price: {hit.price}</p>
+      <img src={hit.image} alt={hit.description} />
     </div>
   );
-}
+};
+
+const MainContent = () => (
+  <div>
+    <h2>Results</h2>
+    <div className='info'>
+      <Stats />
+      <SortBy defaultRefinement='sortby_replica' items={sortByItems} />
+    </div>
+  </div>
+);
+
+//@ts-ignore
+const createURL = (state) => `?${qs.stringify(state)}`;
+//@ts-ignore
+const searchStateToUrl = (searchState) => {
+  return searchState ? `${createURL(searchState)}` : '';
+};
+
+//@ts-ignore
+
+const urlToSearchState = ({ search }) => qs.parse(search.slice(1));
+//@ts-ignore
+
+const App = ({ location }) => {
+  const history = useHistory();
+  const [searchState, setSearchState] = useState(urlToSearchState(location));
+  const [debouncedSetState, setDebouncedSetState] = useState(null);
+  //@ts-ignore
+
+  const onSearchStateChange = (updatedSearchState) => {
+    //@ts-ignore
+    clearTimeout(debouncedSetState);
+    //@ts-ignore
+    setDebouncedSetState(
+      //@ts-ignore
+      setTimeout(() => {
+        //@ts-ignore
+        history.push({
+          pathname: location.pathname,
+          search: searchStateToUrl(updatedSearchState),
+        });
+      }, DEBOUNCE_TIME)
+    );
+
+    setSearchState(updatedSearchState);
+  };
+
+  return (
+    <InstantSearch
+      searchClient={searchClient}
+      indexName='demo_ecommerce'
+      searchState={searchState}
+      onSearchStateChange={onSearchStateChange}
+      createURL={createURL}
+    >
+      <SearchBox />
+      <MainContent />
+      <Hits hitComponent={Hit} />
+      <Pagination showLast />
+    </InstantSearch>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route component={App} path='/phones' />
+      {/* @ts-ignore */}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
